58a3bbaa077439c94f40b0db7c6978c3ef908492
bigfa <jigoulee@gmail.com>
2015-12-04 14:14:29 +0800

    remove

--- functions.php
+++ functions.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 define('LION_VERSION','1.0.2');
-=======
-define('LION_VERSION','1.0.1')
->>>>>>> master
 require get_template_directory() . '/inc/base.php';   
 require get_template_directory() . '/inc/comment.php'; 
 require get_template_directory() . '/inc/widget.php'; 
