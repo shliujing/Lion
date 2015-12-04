5ecad22e536d6db2f3d04c469f6aec6d1303ad70
bigfa <jigoulee@gmail.com>
2015-12-04 14:07:24 +0800

    1.0.2

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-define('LION_VERSION','1.0.1')
+define('LION_VERSION','1.0.2');
 require get_template_directory() . '/inc/base.php';   
 require get_template_directory() . '/inc/comment.php'; 
 require get_template_directory() . '/inc/widget.php'; 
